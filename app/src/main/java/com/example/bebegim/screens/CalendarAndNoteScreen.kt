beb2099149593381915b68bee382ee86aa9140e6
iremmiy <iireemsan@gmail.com>
2025-06-24 11:01:56 +0300

    profil fotoğrafı ekleme kısmı eklendi

--- app/src/main/java/com/example/bebegim/screens/CalendarAndNoteScreen.kt
+++ app/src/main/java/com/example/bebegim/screens/CalendarAndNoteScreen.kt
@@ -14,6 +14,14 @@ import androidx.compose.ui.unit.dp
 import com.example.bebegim.ui.components.BottomNavBar
 import com.example.bebegim.ui.theme.DarkPastelBlue
 import com.example.bebegim.ui.theme.PastelBlueWhite
+import android.app.DatePickerDialog
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import java.util.*
 
 @Composable
 fun CalendarAndNoteScreen(
@@ -60,4 +68,5 @@ fun CalendarAndNoteScreenPreview() {
         onNavigateToReports = {},
         onNavigateToProfile = {}
     )
+
 }
