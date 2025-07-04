f9b4949b82dc3c41bac76098e5e4663057783f80
iremmiy <iireemsan@gmail.com>
2025-07-04 19:43:33 +0300

    Implement Note Repository and ViewModel for user-specific note management

--- app/src/main/java/com/example/bebegim/room/AppDatabase.kt
+++ app/src/main/java/com/example/bebegim/room/AppDatabase.kt
@@ -4,27 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.bebegim.room.DAO.AlertsDao
-import com.example.bebegim.room.DAO.BabiesDao
-import com.example.bebegim.room.DAO.ChatMessagesDao
-import com.example.bebegim.room.DAO.ChatSessionsDao
-import com.example.bebegim.room.DAO.DevelopmentMilestonesDao
-import com.example.bebegim.room.DAO.DocumentEmbeddingsDao
-import com.example.bebegim.room.DAO.FeedingRecordsDao
-import com.example.bebegim.room.DAO.GrowthMeasurementDao
-import com.example.bebegim.room.DAO.MedicalHistoryDao
-import com.example.bebegim.room.DAO.SleepRecordDao
+import com.example.bebegim.room.DAO.*
 import com.example.bebegim.roomEnhance.SystemLogs
-import com.example.bebegim.room.DAO.TemperatureReadingDao
-import com.example.bebegim.room.DAO.UsersDao
-import com.example.bebegim.room.DAO.VaccinationsDao
-
 
 @Database(
     entities = [
         Alerts::class,
         Babies::class,
-        ChatMessages::class ,
+        ChatMessages::class,
         ChatSessions::class,
         DevelopmentMilestones::class,
         DocumentEmbeddings::class,
@@ -41,21 +28,8 @@ import com.example.bebegim.room.DAO.VaccinationsDao
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-
-/*    abstract fun AlertsDao (): AlertsDao*/
     abstract fun BabiesDao(): BabiesDao
-/*    abstract fun ChatMessagesDao(): ChatMessagesDao
-    abstract fun ChatSessionsDao(): ChatSessionsDao
-    abstract fun FeedingRecordsDao(): FeedingRecordsDao*/
     abstract fun UsersDao(): UsersDao
-/*    abstract fun DevelopmentMilestonesDao(): DevelopmentMilestonesDao
-    abstract fun DocumentEmbeddingsDao(): DocumentEmbeddingsDao
-    abstract fun GrowthMeasurementDao(): GrowthMeasurementDao
-    abstract fun MedicalHistoryDao(): MedicalHistoryDao
-    abstract fun SleepRecordDao(): SleepRecordDao
-    abstract fun SystemLogsDao(): SystemLogsDao
-    abstract fun TemperatureReadingDao(): TemperatureReadingDao
-    abstract fun VaccinationsDao(): VaccinationsDao*/
 
     companion object {
         @Volatile
@@ -68,7 +42,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "bebegim_db"
                 )
-                    .fallbackToDestructiveMigration()  // ← Geliştirme aşamasında ekleyin
+                    .fallbackToDestructiveMigration()
                     .build().also { INSTANCE = it }
             }
         }
